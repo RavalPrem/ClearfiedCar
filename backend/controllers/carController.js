d26c0ffba78dfbf9802f0a0db099a8b36909723f
Prem Raval <ravalprem17@gmail.com>
2026-06-18 22:10:13 +0530

    post request added for car

--- backend/controllers/carController.js
+++ backend/controllers/carController.js
@@ -1,34 +1,86 @@
 const mongoose = require("mongoose");
 const bcrypt = require("bcrypt");
 
-const car = require('../models/carCollectionModel')
+const car = require("../models/carCollectionModel");
 
-const getCarInfo = async(req,res) => {
-    try {
-        const carData = await car.findById(req.params.id)
+const getCarInfo = async (req, res) => {
+  try {
+    const carData = await car.findById(req.params.id);
 
-        return res.status(201).json({
-            data : carData,
-            message : success
-        })
+    return res.status(201).json({
+      data: carData,
+      message: success,
+    });
+  } catch (error) {
+    return res.status(500).json({
+      message: error.message,
+      success: false,
+    });
+  }
+};
 
-    } catch (error) {
-        return res.status(500).json({
-            message : error.message,
-            success : false
-        })
+const postCarInfo = async (req, res) => {
+  try {
+    const {
+      brand,
+      model,
+      year,
+      fuelType,
+      transmission,
+      kilometersDriven,
+      price,
+      location,
+      description,
+      carImage,
+      isSold,
+    } = req.body;
+
+    if (
+      !brand ||
+      !model ||
+      !year ||
+      !fuelType ||
+      !transmission ||
+      !kilometersDriven ||
+      !price ||
+      !location ||
+      !description ||
+      !carImage ||
+      !isSold
+    ) {
+      return res.status(401).json({
+        message: "all fields are required",
+        success: false,
+      });
     }
-}
 
-const postCarInfo = async(req, res) => {
-    try {
-        //
-    } catch (error) {
-        return res.status(500).json({
-            message : error.message,
-            success : false
-        })
-    }
-}
+    const newCar = await car.create({
+      sellerId,
+      brand,
+      model,
+      year,
+      fuelType,
+      transmission,
+      kilometersDriven,
+      price,
+      location,
+      description,
+      carImage,
+      isSold
+    });
 
-module.exports = {getCarInfo}
+    return res.status(201).json({
+        message : "car data successfully got",
+        success : true,
+        data : newCar
+    })
+
+  } catch (error) {
+    return res.status(500).json({
+      message: error.message,
+      success: false,
+    });
+  }
+};
+
+module.exports = { getCarInfo,postCarInfo };
--- backend/routes/carRoutes.js
+++ backend/routes/carRoutes.js
@@ -1,11 +1,13 @@
 const express = require('express')
 const mongoose = require('mongoose')
 const Car = require('../models/carCollectionModel')
-const { getCarInfo } = require('../controllers/carController')
+const { getCarInfo, postCarInfo } = require('../controllers/carController')
 
 
 const route = express.Router()
+route.post('/carInfo',postCarInfo)
 
 route.get('/:id', getCarInfo)
+
 
 module.exports = route
